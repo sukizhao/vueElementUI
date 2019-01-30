783b52eb4a1533bbb33176cf5368213108ca2b4f
zhaoshuqi <852056893@qq.com>
2019-01-30 15:43:45 +0800

    vue + Element UI

--- README.md
+++ README.md
@@ -1,33 +1,18 @@
-vue-awesome-swiper的使用
-1、cd到项目里面npm install vue-awesome-swiper --save  安装依赖package.json里面有vue-awesome-swiper即可
-  "dependencies": {
-      "element-ui": "^2.4.11",
-      "vue": "^2.5.11",
-      "vue-awesome-swiper": "^3.1.3"
-    },
-2、去应用轮播图的vue文件引用
-  /*组件方式引用*/
-      import 'swiper/dist/css/swiper.css'////这里注意具体看使用的版本是否需要引入样式，以及具体位置。
-      import { swiper, swiperSlide } from 'vue-awesome-swiper'
-      export default {
-        data() {
-          return {
-              swiperOption: {
-                notNextTick: true,
-                autoplay: true,
-                speed: 500,
-                loop: true
-              },
-            };
-          },
-      components: {
-        swiper,
-        swiperSlide
-      }
-  }
-3、引用轮播的vue写轮播图代码即可
-     <swiper :options="swiperOption">
-      <swiper-slide><img src="../assets/images/xingtong1.jpg" class="lunbotu"></swiper-slide>
-      <swiper-slide><img src="../assets/images/xingtong2.jpg" class="lunbotu"></swiper-slide>
-      <swiper-slide><img src="../assets/images/xingtong3.jpg" class="lunbotu"></swiper-slide>
-    </swiper>
+# vue
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,57 +2,24 @@
 <div>
   <!-- 注意代码必须放在div根节点里面 -->
   <h1>我是home组建</h1>
-  <!-- <el-row>
+  <el-row>
     <el-button>默认按钮</el-button>
     <el-button type="primary">主要按钮</el-button>
     <el-button type="success">成功按钮</el-button>
     <el-button type="info">信息按钮</el-button>
     <el-button type="warning">警告按钮</el-button>
     <el-button type="danger">危险按钮</el-button>
-  </el-row>-->
-
-   <swiper :options="swiperOption">
-      <swiper-slide><img src="../assets/images/xingtong1.jpg" class="lunbotu"></swiper-slide>
-      <swiper-slide><img src="../assets/images/xingtong2.jpg" class="lunbotu"></swiper-slide>
-      <swiper-slide><img src="../assets/images/xingtong3.jpg" class="lunbotu"></swiper-slide>
-    </swiper>
-    <!--以下看需要添加-->
-      <div class="swiper-pagination"></div>
-      <div class="swiper-button-next"></div>
-      <div class="swiper-button-prev"></div>
+  </el-row>
 </div>
 </template>
 <script>
-    /*组件方式引用*/
-    import 'swiper/dist/css/swiper.css';
-    import { swiper, swiperSlide } from 'vue-awesome-swiper';
-    export default {
-         data() {
-      return {
-        swiperOption: {
-           pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-          },
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-          }
-        }
-      }
-    },
-    components: {
-      swiper,
-      swiperSlide
-    }
-}
 
 </script>
 
  <style lang="scss" scoped>
 //  scoped 表示局部作用域
     h1{color:red;}
-    .lunbotu{height:300px;}
+
 </style>
 
 
